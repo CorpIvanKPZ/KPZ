1cec0e02a818b02e290981f235a015bdc38a5ae5
CorpIvanKPZ <152106416+CorpIvanKPZ@users.noreply.github.com>
2023-11-27 00:41:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import PySimpleGUI as sg
 import logic
-from tabulate import tabulate
 
 def main():
     sg.theme('LightBlue2')
@@ -9,7 +8,7 @@ def main():
         [sg.Text("Ласкаво просимо до гри 'Вгадай число'!", size=(30, 1), justification='center')],
         [sg.Text("Введіть число між 1 та 100:", size=(30, 1), justification='center')],
         [sg.InputText(size=(10, 1), key='-INPUT-')],
-        [sg.Button('Вгадати'), sg.Button('Вийти'), sg.Button('Рекорди')],
+        [sg.Button('Вгадати'), sg.Button('Підказка'), sg.Button('Вийти')],
         [sg.Text('', size=(30, 1), key='-OUTPUT-', justification='center')],
     ]
 
@@ -22,12 +21,13 @@ def main():
 
         if event in (sg.WINDOW_CLOSED, 'Вийти'):
             break
-        elif event == 'Рекорди':
-            scores_table = logic.get_scores_table()
-            scores_str = tabulate(scores_table, headers=['Місце', 'Спроби'], tablefmt='pretty')
-            sg.popup_scrolled(scores_str, title='Рекорди')
+        elif event == 'Вгадати':
+            result = logic.play_round(values['-INPUT-'])
+        elif event == 'Підказка':
+            result = logic.hint()
+            window['-OUTPUT-'].update(result)
+            continue  # Продовжуємо цикл без виклику play_round
 
-        result = logic.play_round(values['-INPUT-'])
         window['-OUTPUT-'].update(result)
 
     window.close()
