58b3bdf06887ad790ad94f0c3984198bec36b43e
CorpIvanKPZ <152106416+CorpIvanKPZ@users.noreply.github.com>
2023-11-27 00:41:30 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,23 @@
 import json
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
 
 scores = []
 
 def load_scores():
     global scores
     try:
-        with open("scores.json", "r") as file:
-            scores = json.load(file)
-    except FileNotFoundError:
+        with open("scores.json", "rb") as file:
+            encrypted_data = file.read()
+            decrypted_data = cipher_suite.decrypt(encrypted_data)
+            scores = json.loads(decrypted_data)
+    except (FileNotFoundError, json.JSONDecodeError):
         scores = []
 
 def save_score(attempts):
     scores.append(attempts)
-    with open("scores.json", "w") as file:
-        json.dump(scores, file)
+    encrypted_data = cipher_suite.encrypt(json.dumps(scores).encode())
+    with open("scores.json", "wb") as file:
+        file.write(encrypted_data)
